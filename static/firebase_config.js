17bb49cdae2cbfae716ad74f4f26a196fcc0345e
Nisarg_Debian <amlaninisarg15@gmail.com>
2024-02-13 01:57:12 +0530

    added code for sign up

--- static/firebase_config.js
+++ static/firebase_config.js
@@ -1,26 +1,4 @@
 
-    // Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
-    import { getAuth, createUserWithEmailAndPassword , signInWithEmailAndPassword} from "firebase/auth";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-    apiKey: "AIzaSyDoKKxEn-IChCtdV8BuoohMf7TMZyZXZxk",
-    authDomain: "educonnect-6789e.firebaseapp.com",
-    projectId: "educonnect-6789e",
-    storageBucket: "educonnect-6789e.appspot.com",
-    messagingSenderId: "422805237793",
-    appId: "1:422805237793:web:7fcf5afa6d241f935b3360",
-    measurementId: "G-XHXM785F62"
-};
-
-    // Initialize Firebase
-    const login = {getAuth , createUserWithEmailAndPassword , signInWithEmailAndPassword};
-    const app = initializeApp(firebaseConfig);
-    const auth = getAuth(app);
     export {auth}
     export {login}
 
--- static/login.js
+++ static/login.js
@@ -1,29 +1,63 @@
-import {login} from "./firebase_config";
-import {auth} from "./firebase_config";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-document.addEventListener("DOMContentLoaded", function() {
-    const container=document.getElementById('container')
-    const signIn=document.getElementById('signIn-updated')
-    const signUp=document.getElementById('signup-updated')
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDoKKxEn-IChCtdV8BuoohMf7TMZyZXZxk",
+	authDomain: "educonnect-6789e.firebaseapp.com",
+	projectId: "educonnect-6789e",
+	storageBucket: "educonnect-6789e.appspot.com",
+	messagingSenderId: "422805237793",
+	appId: "1:422805237793:web:7fcf5afa6d241f935b3360",
+	measurementId: "G-XHXM785F62",
+};
 
-    signUp.addEventListener('click',function () {
-        container.classList.add("right-active")
-        // console.log("added");
-    })
+// Initialize Firebase
+const login = {
+	getAuth,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword,
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+document.addEventListener("DOMContentLoaded", function () {
+	const container = document.getElementById("container");
+	const signIn = document.getElementById("signIn-updated");
+	const signUp = document.getElementById("signup-updated");
 
-    signIn.addEventListener('click',function () {
-        container.classList.remove("right-active")
-        // console.log("removed");
-    })
+	signUp.addEventListener("click", function () {
+		container.classList.add("right-active");
+		// console.log("added");
+	});
 
-    const form = document.getElementById("formSignup");
-    // console.log(form);
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
+	signIn.addEventListener("click", function () {
+		container.classList.remove("right-active");
+		// console.log("removed");
+	});
 
-        const username = document.getElementById("nameId").value;
-        // console.log(username);
-        // Redirect to the next page with the username as a query parameter
-        window.location.href = `../../index.html?username=${username}`;
-    });
+	const signup = document.getElementById("formSignup");
+	signup.addEventListener("submit", function (event) {
+		event.preventDefault();
+		const name = signup["name"].value; // Corrected from 'Name' to 'name'
+		const email = signup["email"].value; // Corrected from 'Email' to 'email'
+		const password = signup["password"].value; // Corrected from 'Password' to 'password'
+		console.log(name, email, password);
+
+		createUserWithEmailAndPassword(auth, email, password)
+			.then((userCredential) => {
+				window.location.href = "index.html";
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+			});
+	});
 });
