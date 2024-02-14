8986b2893886ebf3e95bcb3ea574685d6678059c
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2024-02-14 22:27:19 +0530

    mysql added

--- final_project/settings.py
+++ final_project/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EduConnect',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
