e216ec4c86337a6708f03a30eaaf2dd57d274ba7
Nisarg_Amlani_Kali <amlaninisarg15@gmail.com>
2024-03-28 10:00:53 +0530

    added code for view button

--- final_project/views.py
+++ final_project/views.py
@@ -93,8 +93,27 @@ def assignment(request, course_name):
         context = {'course_name': course_name, 'assignments': assignments, "current_date": current_date}
 
     elif request.user_role == "Student":
-        assignments = Assignment.objects.prefetch_related('submission_set')
-        context = {'course_name': course_name, 'assignments': assignments, "current_date": current_date}
+        sub_set = {}
+        assignments = Assignment.objects.filter(class_name__subject=course_name).prefetch_related('submission_set')
+        asmt = Assignment.objects.filter(class_name__subject=course_name)
+
+        cnt = 0
+        submission_link = ''
+        for x in assignments:
+            print(x.submission_set.filter(student_id=request.user_id).first())
+            obj = x.submission_set.filter(student_id=request.user_id).first()
+            if obj is not None:
+                submission_link = obj.link
+            else:
+                print(None)
+                submission_link = None
+
+            sub_set[x.asgmt_id] = submission_link
+
+        print(sub_set)
+
+        context = {'course_name': course_name, 'assignments': asmt, "current_date": current_date,
+                   "sub_set": sub_set}
 
     return render(request, "assignment.html", context)
 
