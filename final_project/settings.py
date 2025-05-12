e4ba9022f1f03638d64bd5cbf1bc0f11de7c716a
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-05-12 20:55:19 +0530

    some changes for deployment

--- final_project/settings.py
+++ final_project/settings.py
@@ -29,6 +29,8 @@ ALLOWED_HOSTS = [PRODUCTION_HOST]
 
 # Application definition
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "final_project.middleware.UserDetailsMiddleware",
 ]
 
@@ -121,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
